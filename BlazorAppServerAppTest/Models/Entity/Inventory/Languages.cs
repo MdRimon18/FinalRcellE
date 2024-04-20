079fd141c678d6668d7ec4915e4473881145caf7
mohimamou <mohiuzumaki24@gmail.com>
2024-04-20 09:24:07 +0600

    currency and language service ready

---
+++ BlazorAppServerAppTest/Models/Entity/Inventory/Languages.cs
@@ -0,0 +1,19 @@
+﻿namespace Pms.Models.Entity.Inventory
+{
+    public class Languages
+    {
+        public class Language
+        {
+            public int LanguageId { get; set; }
+            public Guid? LanguageKey { get; set; }
+            public string LanguageName { get; set; }
+            public DateTime EntryDateTime { get; set; }
+            public long EntryBy { get; set; }
+            public DateTime? LastModifyDate { get; set; }
+            public long? LastModifyBy { get; set; }
+            public DateTime? DeletedDate { get; set; }
+            public long? DeletedBy { get; set; }
+            public string Status { get; set; }
+        }
+    }
+}
---
+++ BlazorAppServerAppTest/Models/Entity/Inventory/StatusSettings.cs
@@ -0,0 +1,22 @@
+﻿namespace Pms.Models.Entity.Inventory
+{
+    public class StatusSettings
+    {
+        public class StatusSetting
+        {
+            public long StatusSettingId { get; set; }
+            public Guid? StatusSettingKey { get; set; }
+            public long BranchId { get; set; }
+            public string StatusSettingName { get; set; }
+            public string PageName { get; set; }
+            public int? Position { get; set; }
+            public DateTime EntryDateTime { get; set; }
+            public long EntryBy { get; set; }
+            public DateTime? LastModifyDate { get; set; }
+            public long? LastModifyBy { get; set; }
+            public DateTime? DeletedDate { get; set; }
+            public long? DeletedBy { get; set; }
+            public string Status { get; set; }
+        }
+    }
+}
