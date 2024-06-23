76bd3b4dab5c72dd9b9d9e0cd83b8796343df2df
Md Rimon <rimoncse18@gmail.com>
2024-06-23 18:24:40 +0600

    update

---
+++ BlazorAppServerAppTest/Models/Entity/Settings/Customers.cs
@@ -0,0 +1,26 @@
+﻿namespace Pms.Models.Entity.Settings
+{
+    public class Customers
+    {
+        public long CustomerId { get; set; }
+        public Guid? CustomerKey { get; set; }
+        public long? BranchId { get; set; }
+        public string CustomerName { get; set; }
+        public string MobileNo { get; set; }
+        public string Email { get; set; }
+        public long CountryId { get; set; }
+        public string StateName { get; set; }
+        public string CustAddrssOne { get; set; }
+        public string CustAddrssTwo { get; set; }
+        public string Occupation { get; set; }
+        public string OfficeName { get; set; }
+        public string CustImgLink { get; set; }
+        public DateTime? EntryDateTime { get; set; }
+        public long? EntryBy { get; set; }
+        public DateTime? LastModifyDate { get; set; }
+        public long? LastModifyBy { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public long? DeletedBy { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ BlazorAppServerAppTest/Models/Entity/Settings/Suppliers.cs
@@ -0,0 +1,27 @@
+﻿namespace Pms.Models.Entity.Settings
+{
+    public class Suppliers
+    {
+        public long SupplierId { get; set; }
+        public Guid? SupplierKey { get; set; }
+        public long? BranchId { get; set; }
+        public string SupplrName { get; set; }
+        public string MobileNo { get; set; }
+        public string Email { get; set; }
+        public string SuppOrgnznName { get; set; }
+        public long CountryId { get; set; }
+        public string StateName { get; set; }
+        public long BusinessTypeId { get; set; }
+        public string SupplrAddrssOne { get; set; }
+        public string SupplrAddrssTwo { get; set; }
+        public string DeliveryOffDay { get; set; }
+        public string SupplrImgLink { get; set; }
+        public DateTime? EntryDateTime { get; set; }
+        public long? EntryBy { get; set; }
+        public DateTime? LastModifyDate { get; set; }
+        public long? LastModifyBy { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public long? DeletedBy { get; set; }
+        public string Status { get; set; }
+    }
+}
